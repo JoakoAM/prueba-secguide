fa25bbed033d0035e04e7c16fc1ba39a886029e6
JoakoAM <jomachuca@cftsa.cl>
2025-10-21 17:16:43 -0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-
 import Hub from "./pages/Hub";
 // import { useState, type FormEvent } from "react";
 // import UserFormProvider from "./providers/UserFormProvider";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
